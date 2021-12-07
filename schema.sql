439865c0f91196c905ecf6ecc1e8db532ec3fcf0
Meet10enten <65298048+tufoinnkuo10@users.noreply.github.com>
2021-12-07 20:17:27 +0100

    Update schema.sql
    add new species column

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT NOT NULL,
+    name varchar(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
+    species string,
+    PRIMARY KEY (id)
+
 );
