78b66794cd43738432c9048c6959c6a13cb69d5a
Meet10enten <65298048+tufoinnkuo10@users.noreply.github.com>
2021-12-07 13:35:33 +0100

    Update queries.sql
    add different commands to get different data from the table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT name from animals WHERE neutered = 'yes' AND escape_attempts < 3;
+SELECT date_of_birth from animals WHERE name='Agumon' OR name='Pikachu';
+SELECT name, escape_attempts from animals WHERE weight_kg>10.5;
+SELECT name from animals where neutered=TRUE;
+SELECT name from animals where name='Gabumon';
+SELECT name from animals where weight_kg BETWEEN 10.4 AND 17.3;
