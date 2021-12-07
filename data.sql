a7a085cb33fc9b33b149f6943a0a31518c898ac4
Meet10enten <65298048+tufoinnkuo10@users.noreply.github.com>
2021-12-07 20:35:11 +0100

    Update data.sql
    add different animals and species

--- data.sql
+++ data.sql
@@ -1,7 +1,13 @@
 INSERT INTO animals 
-(id,name,date_of_birth,escape_attempts,neutered,weight_kg)
+(id,name,date_of_birth,escape_attempts,neutered,weight_kg,species)
 VALUES 
 ('1','Agumon','02-03-2020','1','yes','10.23');
 ('2','Gabumon','11-15-2018','1','no','8');
 ('3','Pikachu','01-07-2021','5','yes','15.04');
 ('4','Devimon','03-12-2017','5','yes','11');
+('5','Charmander','02-08-2020','0','No','-11','Domestic');
+('6','Plantmon','11-15-2022','2','true','-5.7','wild');
+('7','Squirtle','04-02-1993','3','false','-12.13','very wild');
+('8','Angemon','06-12-2005','1','true','-45','Domestic');
+('9','Boarmon','06-07-2005','7','true','20.4','wise mix');
+('10','Blossom','10-13-1998','3','true','17','Domestic');
